4741617ded3add2c261bc15a9868beba90ce2291
Yahweh's Delight <intricatesyllable@gmail.com>
2024-10-17 00:13:46 +0300

    1-country_users.sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -6,7 +6,7 @@
 -- country, enumeration of countries: US, CO and TN, never null (= default will be the first element of the enumeration, here US)
 -- If the table already exists, your script should not fail
 
-CREATE TABLE users (id integer NOT NULL AUTO_INCREMENT, PRIMARY KEY (id),
-                   email varchar(255) NOT NULL UNIQUE,
-                   name varchar(255),
+CREATE TABLE IF NOT EXISTS users (id integer NOT NULL AUTO_INCREMENT, PRIMARY KEY (id),
+                   email VARCHAR(255) NOT NULL UNIQUE,
+                   name VARCHAR(255),
 		   country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US');
